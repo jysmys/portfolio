9489e0890488a83d2573bec025b34b6246691655
japh <jenny.scherr@svenskaspel.se>
2020-04-16 10:27:17 +0200

    added footer and header to the page. Also cypress test for that.

--- webpack.config.js
+++ webpack.config.js
@@ -3,42 +3,44 @@ const webpack = require("webpack");
 const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js", // tells us where the app starts and where to bundle the files
-  mode: "development", // tells webpack to know im workin in development mode
+  entry: "./src/index.js",
+  mode: "development",
   module: {
-    // define how js modules are transformed
     rules: [
-      // tells which modules that are included
       {
         test: /\.(js|jsx)$/,
-        exclude: /(node_modules)/, // it will match anything that is not in node_modules
+        exclude: /(node_modules)/,
         loader: "babel-loader",
         options: { presets: ["@babel/env"] },
       },
       {
-        test: /\.css$/, // defines how we want to  process CSS
+        test: /\.css$/,
         use: ["style-loader", "css-loader"],
       },
     ],
   },
-  resolve: { extensions: ["*", ".js", ".jsx"] }, // the extensions Webpack will resolve. Here it's possible to import modules without adding the extensions for them
+  resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
-    // where Webpack should put the transpiled and boundled code
     path: path.resolve(__dirname, "dist/"),
-    publicPath: "/dist/", // the directory the boundle should go in and where the webpack server should find the files
+    publicPath: "/dist/",
     filename: "bundle.js",
   },
   devServer: {
-    // this configures the webpack-dev-server and where the static files are
     contentBase: path.join(__dirname, "/"),
-    port: 3000, // which port the server should run on
+    port: 3000,
     publicPath: "http://localhost:3000/dist/",
     watchContentBase: true,
     historyApiFallback: true,
   },
   optimization: {
     minimizer: [
-      new UglifyJsPlugin({ uglifyOptions: { mangle: { keep_fnames: true } } }),
+      new UglifyJsPlugin({
+        uglifyOptions: {
+          mangle: {
+            keep_fnames: true,
+          },
+        },
+      }),
     ],
   },
   plugins: [new webpack.HotModuleReplacementPlugin()],
